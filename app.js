a13bd00d73d7ffb30ba424da0454b508f59779cd
Jefferson Gomes <jefferson.g.silva@hotmail.com>
2019-03-07 18:18:41 -0300

    instalado módulo body-parser e criada rota post de admin/cadastroProduto

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express= require('express');
 const consign= require ('consign');
+const bodyparser= require('body-parser');
 
 const app= express();
 
 app.set('view engine','ejs');
 app.set('views','aplication/views');
 
+app.use(bodyparser.urlencoded({ extended : true }));
 
 consign().include('aplication/rotas').then('aplication/controller').then('aplication/model').into(app);
 app.listen(3000,()=>{
