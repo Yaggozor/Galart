b9b757f14c4bc65d8713c4bf663a42ecd547deab
Jefferson Gomes <jefferson.g.silva@hotmail.com>
2019-05-19 01:13:11 -0300

    Alterações para deploy no heroku

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ consign()
     .then("pagSeguroConfig/pagSeguro.js")
     .into(app);
 
-app.listen(3000,()=>{
+const PORT = process.env.PORT || 3000
+app.listen(PORT,()=>{
     console.log('Agora vai')
 });
