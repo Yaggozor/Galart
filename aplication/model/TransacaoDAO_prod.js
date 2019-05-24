307d2c46eb9dd50c9d90313567f708552aceafde
Jefferson Gomes <jefferson.g.silva@hotmail.com>
2019-05-23 22:34:36 -0300

    função enviar img p/ server e delete comentários

--- aplication/model/TransacaoDAO_prod.js
+++ aplication/model/TransacaoDAO_prod.js
@@ -55,37 +55,6 @@ TransacaoDAO.prototype.mostrarCompra = function (req, res) {
     });
 }
 
-/*TransacaoDAO.prototype.inserirTransacao = function (transacao) {
-    this._conexao.open(function (err, mongoclient) {
-        mongoclient.collection("transacoes", function (err, collection) {
-            collection.insertOne(transacao);
-        });
-        mongoclient.close();
-    });
-}
-
-TransacaoDAO.prototype.mostrarTransacao = function (res) {
-    this._conexao.open(function (err, mongoclient) {
-        mongoclient.collection("transacoes", function (err, collection) {
-            collection.find().toArray(function (err, result) {
-                res.render("admin/historicoTransacao", { data: result });
-            });
-        });
-        mongoclient.close();
-    });
-}
-
-TransacaoDAO.prototype.mostrarCompra = function (req, res) {
-    this._conexao.open(function (err, mongoclient) {
-        mongoclient.collection("transacoes", function (err, collection) {
-            collection.find({ email : req.session.email }).toArray(function (err, result) {
-                res.render("cliente/historicoCompra", { data: result });
-            });
-        });
-        mongoclient.close();
-    });
-}*/
-
 module.exports = () => {
     return TransacaoDAO;
 }
